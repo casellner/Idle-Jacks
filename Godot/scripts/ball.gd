711754ca5e89860dba3012c27ed9b3eddac6f745
casellner <114782256+casellner@users.noreply.github.com>
2025-01-24 00:07:04 -0500

    Add jack that spawns when ball is bounced and despawns when ball hits ground

--- Godot/scripts/ball.gd
+++ Godot/scripts/ball.gd
@@ -4,14 +4,17 @@ const BOUNCE_VELOCITY = -800.0 # speed at which ball is bounced when clicked
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+var jump
+
 # Gravity physics
 func _physics_process(delta):
 	# Affect ball by gravity
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		
-	# Handle jump
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = BOUNCE_VELOCITY
+	# if jump command was sent by game.gd
+	if jump:
+		jump = false # make jump command false
+		velocity.y = BOUNCE_VELOCITY # give ball upward velocity
 	
 	move_and_slide()
