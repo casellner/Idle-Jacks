f46ccacf34732f6309237c69ee31cb7ad22bf749
casellner <114782256+casellner@users.noreply.github.com>
2025-01-27 20:01:14 -0500

    Add comments to jack_button.gd

--- Godot/jack_button.gd
+++ Godot/jack_button.gd
@@ -1,10 +1,19 @@
 extends TextureButton
 
+"""
+"    Texture randomization code adapted from godot-texture-variation-tutorial
+"    Ivan Shyika
+"    Accessed 1/27/2025
+"    https://github.com/IvessJohn/godot-texture-variation-tutorial
+"""
+
+# array of normal (not pressed) jack button textures
 @export var TEXTURE_NORMAL_VARIATIONS_ARRAY: Array = [
 	preload("res://sprites/jack.png"),
 	preload("res://sprites/jock.png")
 ]
 
+# array of pressed jack button textures
 @export var TEXTURE_PRESSED_VARIATIONS_ARRAY: Array = [
 	preload("res://sprites/jack90.png"),
 	preload("res://sprites/jock90.png")
@@ -12,6 +21,7 @@ extends TextureButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	# pick random texture
 	var texture_id: int = randi() % TEXTURE_NORMAL_VARIATIONS_ARRAY.size()
 	var chosen_texture_normal: Texture = TEXTURE_NORMAL_VARIATIONS_ARRAY[texture_id]
 	var chosen_texture_pressed: Texture = TEXTURE_PRESSED_VARIATIONS_ARRAY[texture_id]
